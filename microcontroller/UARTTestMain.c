5c7f34df6dcf9b50421044138d603297fd7c1751
rfid-ie-cet-2017-project <sunilpatra19@gmail.com>
2017-05-02 00:28:38 +0530

    Update UARTTestMain.c

--- microcontroller/UARTTestMain.c
+++ microcontroller/UARTTestMain.c
@@ -1,29 +1,7 @@
 // UARTTestMain.c
 // Runs on LM4F120/TM4C123
-// Used to test the UART.c driver
-// Daniel Valvano
-// September 12, 2013
 
-/* This example accompanies the book
-   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
-   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2013
 
- Copyright 2013 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
-
-// U0Rx (VCP receive) connected to PA0
-// U0Tx (VCP transmit) connected to PA1
-
-//led below
 #define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
 #define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
 #define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
@@ -102,32 +80,3 @@ int main(void){
 			
 	}
 }
-	/*
-	while(1)
-  for(i='a'; i<='z'; i=i+1){// print the lowercase alphabet
-    UART_OutChar(i);
-		j=100000000;
-		while(j--);
-  }
-  OutCRLF();
-	
-	
-	
-  UART_OutChar('-');
-  UART_OutChar('-');
-  UART_OutChar('>');
-  while(1){
-    UART_OutString("InString: ");
-    UART_InString(string,19);
-    UART_OutString(" OutString="); UART_OutString(string); OutCRLF();
-
-    UART_OutString("InUDec: ");  n=UART_InUDec();
-    UART_OutString(" OutUDec="); UART_OutUDec(n); OutCRLF();
-
-    UART_OutString("InUHex: ");  n=UART_InUHex();
-    UART_OutString(" OutUHex="); UART_OutUHex(n); OutCRLF();
-
-	}
-  
-}
-*/
